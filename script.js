ef5e0dbb9351056e035d31a26c74cb4670547900
ronniebirolim <ronniebirolim@gmail.com>
2024-10-02 23:04:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-// Função que redireciona para o link do WhatsApp após 5 segundos (5000 ms)
+let progressBar = document.querySelector('.progress');
+let totalTime = 5000; // 5 segundos
+let intervalTime = 50; // Atualiza a cada 50ms
+let increment = intervalTime / totalTime * 100;
+let width = 0;
+
+let progressInterval = setInterval(() => {
+    width += increment;
+    progressBar.style.width = width + '%';
+    if (width >= 100) {
+        clearInterval(progressInterval);
+    }
+}, intervalTime);
+
+// Redireciona para o WhatsApp após 5 segundos
 setTimeout(function() {
     window.location.href = "https://chat.whatsapp.com/AtgD0NW61ls2luONB3WoOI";
-}, 5000);
+}, totalTime);
