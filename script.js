02457a20984a8f37bc91295847c8ecaa2ec7d3a4
ronniebirolim <ronniebirolim@gmail.com>
2024-10-02 22:55:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Função que redireciona para o link do WhatsApp após 5 segundos (5000 ms)
+setTimeout(function() {
+    window.location.href = "https://chat.whatsapp.com/AtgD0NW61ls2luONB3WoOI";
+}, 5000);
